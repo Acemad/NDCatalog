f79e96de9744f778e22cc1d52cfd72cc8fbe2074
Ouessai Abdessamed <ouessai.aes@gmail.com>
2018-11-24 14:11:03 +0100

    Add models.py containing DB models

---
+++ models.py
@@ -0,0 +1,30 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+class User(db.Model):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), nullable=False)
+    email = db.Column(db.String, nullable=False)
+    picture = db.Column(db.String)
+
+
+class Author(db.Model):
+    __tablename__ = 'author'
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String, nullable=False)
+    last_name = db.Column(db.String, nullable=False)
+
+
+class Book(db.Model):
+    __tablename__ = 'book'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String)    
+    tech_category = db.Column(db.String)
+    publish_date = db.Column(db.Date)
+    author_id = db.Column(db.Integer, db.ForeignKey('author.id'))
+    link = db.Column(db.String)
+
+    author = db.relationship(Author)
