ca0356ef603189befd7f6d89ad148feadf8b8552
Unknown <ouessai.aes@gmail.com>
2018-11-30 23:10:56 +0100

    Modify structure of the database

--- models.py
+++ models.py
@@ -11,19 +11,18 @@ class User(db.Model):
     picture = db.Column(db.String)
 
 
-class Author(db.Model):
-    __tablename__ = 'author'
-    id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.String, nullable=False)
-    last_name = db.Column(db.String, nullable=False)
-    bio = db.Column(db.String)
-
-
 class Category(db.Model):
     __tablename__ = 'category'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False, unique=True)
     description = db.Column(db.String)
+
+    @property
+    def serialize(self):
+        return {
+            'name': self.name,
+            'description': self.description
+        }
 
 
 class Book(db.Model):
@@ -35,8 +34,36 @@ class Book(db.Model):
     link = db.Column(db.String)
     cover_url = db.Column(db.String)
     isbn = db.Column(db.String)
-    author_id = db.Column(db.Integer, db.ForeignKey('author.id'))
     category_id = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=False)
-   
-    author = db.relationship(Author)
+    
     category = db.relationship(Category)
+
+    @property
+    def serialize(self):
+        return {
+            'title': self.title,
+            'summary': self.summary,
+            'publish_year': self.publish_year,
+            'link': self.link,
+            'cover_url': self.cover_url,
+            'isbn': self.isbn
+        }
+
+
+class Author(db.Model):
+    __tablename__ = 'author'
+    id = db.Column(db.Integer, primary_key=True)
+    book_id = db.Column(db.Integer, db.ForeignKey('book.id'), nullable=False, )
+    first_name = db.Column(db.String, nullable=False)
+    last_name = db.Column(db.String, nullable=False)    
+
+    book = db.relationship(Book)
+    
+    @property
+    def serialize(self):
+        return {
+            'first_name': self.first_name,
+            'last_name': self.last_name
+        }
+        
+        
